96cb02688ebb6259db91adcd1d8397c4a46361e6
pndnism <p.dai.man8@gmail.com>
2024-03-23 09:23:28 +0900

    add base.py

---
+++ scripts/base.py
@@ -0,0 +1,295 @@
+import os
+from pathlib import Path
+import gc
+from glob import glob
+import pandas as pd
+import polars as pl
+from datetime import datetime
+import warnings
+from sklearn.model_selection import StratifiedGroupKFold
+from sklearn.metrics import roc_auc_score
+import lightgbm as lgb
+import wandb
+import pickle
+
+warnings.filterwarnings('ignore')
+
+
+# start a new wandb run to track this script
+wandb.init(
+    # set the wandb project where this run will be logged
+    project="home-credit",
+    config={
+        "boosting_type": "gbdt",
+        "objective": "binary",
+        "metric": "auc",
+        "max_depth": 10,
+        "learning_rate": 0.05,
+        "n_estimators": 200,  # 1000:0.55
+        "colsample_bytree": 0.8,
+        "colsample_bynode": 0.8,
+        "verbose": -1,
+        "random_state": 42,
+        "reg_alpha": 0.1,
+        "reg_lambda": 10,
+        "extra_trees": True,
+        'num_leaves': 64,
+        # "device": "gpu",
+    }
+)
+
+config = wandb.config
+
+
+class Pipeline:
+
+    def set_table_dtypes(df):
+        for col in df.columns:
+            if col in ["case_id", "WEEK_NUM", "num_group1", "num_group2"]:
+                df = df.with_columns(pl.col(col).cast(pl.Int64))
+            elif col in ["date_decision"]:
+                df = df.with_columns(pl.col(col).cast(pl.Date))
+            elif col[-1] in ("P", "A"):
+                df = df.with_columns(pl.col(col).cast(pl.Float64))
+            elif col[-1] in ("M",):
+                df = df.with_columns(pl.col(col).cast(pl.String))
+            elif col[-1] in ("D",):
+                df = df.with_columns(pl.col(col).cast(pl.Date))
+        return df
+
+    def handle_dates(df):
+        for col in df.columns:
+            if col[-1] in ("D",):
+                df = df.with_columns(pl.col(col) - pl.col("date_decision"))
+                df = df.with_columns(pl.col(col).dt.total_days())
+        df = df.drop("date_decision", "MONTH")
+        return df
+
+    def filter_cols(df):
+        for col in df.columns:
+            if col not in ["target", "case_id", "WEEK_NUM"]:
+                isnull = df[col].is_null().mean()
+                if isnull > 0.95:
+                    df = df.drop(col)
+
+        for col in df.columns:
+            if (col not in ["target", "case_id", "WEEK_NUM"]) & (df[col].dtype == pl.String):
+                freq = df[col].n_unique()
+                if (freq == 1) | (freq > 200):
+                    df = df.drop(col)
+
+        return df
+
+
+class Aggregator:
+
+    def num_expr(df):
+        cols = [col for col in df.columns if col[-1] in ("P", "A")]
+        expr_max = [pl.max(col).alias(f"max_{col}") for col in cols]
+        return expr_max
+
+    def date_expr(df):
+        cols = [col for col in df.columns if col[-1] in ("D")]
+        expr_max = [pl.max(col).alias(f"max_{col}") for col in cols]
+        return expr_max
+
+    def str_expr(df):
+        cols = [col for col in df.columns if col[-1] in ("M",)]
+        expr_max = [pl.max(col).alias(f"max_{col}") for col in cols]
+        return expr_max
+
+    def other_expr(df):
+        cols = [col for col in df.columns if col[-1] in ("T", "L")]
+        expr_max = [pl.max(col).alias(f"max_{col}") for col in cols]
+        return expr_max
+
+    def count_expr(df):
+        cols = [col for col in df.columns if "num_group" in col]
+        # max & replace col name
+        expr_max = [pl.max(col).alias(f"max_{col}") for col in cols]
+        return expr_max
+
+    def get_exprs(df):
+        exprs = Aggregator.num_expr(df) + \
+                Aggregator.date_expr(df) + \
+                Aggregator.str_expr(df) + \
+                Aggregator.other_expr(df) + \
+                Aggregator.count_expr(df)
+
+        return exprs
+
+
+def read_file(path, depth=None):
+    df = pl.read_parquet(path)
+    df = df.pipe(Pipeline.set_table_dtypes)
+    if depth in [1, 2]:
+        df = df.group_by("case_id").agg(Aggregator.get_exprs(df))
+    return df
+
+
+def read_files(regex_path, depth=None):
+    chunks = []
+
+    for path in glob(str(regex_path)):
+        df = pl.read_parquet(path)
+        df = df.pipe(Pipeline.set_table_dtypes)
+        if depth in [1, 2]:
+            df = df.group_by("case_id").agg(Aggregator.get_exprs(df))
+        chunks.append(df)
+
+    df = pl.concat(chunks, how="vertical_relaxed")
+    df = df.unique(subset=["case_id"])
+    return df
+
+
+def feature_eng(df_base, depth_0, depth_1, depth_2):
+    df_base = (
+        df_base
+        .with_columns(
+            month_decision=pl.col("date_decision").dt.month(),
+            weekday_decision=pl.col("date_decision").dt.weekday(),
+        )
+    )
+    for i, df in enumerate(depth_0 + depth_1 + depth_2):
+        df_base = df_base.join(df, how="left", on="case_id", suffix=f"_{i}")
+    df_base = df_base.pipe(Pipeline.handle_dates)
+    return df_base
+
+
+def to_pandas(df_data, cat_cols=None):
+    df_data = df_data.to_pandas()
+    if cat_cols is None:
+        cat_cols = list(df_data.select_dtypes("object").columns)
+    df_data[cat_cols] = df_data[cat_cols].astype("category")
+    return df_data, cat_cols
+
+
+# ROOT = Path("/kaggle/input/home-credit-credit-risk-model-stability")
+ROOT = Path("../data")
+
+TRAIN_DIR = ROOT / "parquet_files" / "train"
+TEST_DIR = ROOT / "parquet_files" / "test"
+
+data_store = {
+    "df_base": read_file(TRAIN_DIR / "train_base.parquet"),
+    "depth_0": [
+        read_file(TRAIN_DIR / "train_static_cb_0.parquet"),
+        read_files(TRAIN_DIR / "train_static_0_*.parquet"),
+    ],
+    "depth_1": [
+        read_files(TRAIN_DIR / "train_applprev_1_*.parquet", 1),
+        read_file(TRAIN_DIR / "train_tax_registry_a_1.parquet", 1),
+        read_file(TRAIN_DIR / "train_tax_registry_b_1.parquet", 1),
+        read_file(TRAIN_DIR / "train_tax_registry_c_1.parquet", 1),
+        read_files(TRAIN_DIR / "train_credit_bureau_a_1_*.parquet", 1),
+        read_file(TRAIN_DIR / "train_credit_bureau_b_1.parquet", 1),
+        read_file(TRAIN_DIR / "train_other_1.parquet", 1),
+        read_file(TRAIN_DIR / "train_person_1.parquet", 1),
+        read_file(TRAIN_DIR / "train_deposit_1.parquet", 1),
+        read_file(TRAIN_DIR / "train_debitcard_1.parquet", 1),
+    ],
+    "depth_2": [
+        read_file(TRAIN_DIR / "train_credit_bureau_b_2.parquet", 2),
+        read_files(TRAIN_DIR / "train_credit_bureau_a_2_*.parquet", 2),
+    ]
+}
+
+df_train = feature_eng(**data_store)
+print("train data shape:\t", df_train.shape)
+
+data_store = {
+    "df_base": read_file(TEST_DIR / "test_base.parquet"),
+    "depth_0": [
+        read_file(TEST_DIR / "test_static_cb_0.parquet"),
+        read_files(TEST_DIR / "test_static_0_*.parquet"),
+    ],
+    "depth_1": [
+        read_files(TEST_DIR / "test_applprev_1_*.parquet", 1),
+        read_file(TEST_DIR / "test_tax_registry_a_1.parquet", 1),
+        read_file(TEST_DIR / "test_tax_registry_b_1.parquet", 1),
+        read_file(TEST_DIR / "test_tax_registry_c_1.parquet", 1),
+        read_files(TEST_DIR / "test_credit_bureau_a_1_*.parquet", 1),
+        read_file(TEST_DIR / "test_credit_bureau_b_1.parquet", 1),
+        read_file(TEST_DIR / "test_other_1.parquet", 1),
+        read_file(TEST_DIR / "test_person_1.parquet", 1),
+        read_file(TEST_DIR / "test_deposit_1.parquet", 1),
+        read_file(TEST_DIR / "test_debitcard_1.parquet", 1),
+    ],
+    "depth_2": [
+        read_file(TEST_DIR / "test_credit_bureau_b_2.parquet", 2),
+        read_files(TEST_DIR / "test_credit_bureau_a_2_*.parquet", 2),
+    ]
+}
+
+df_test = feature_eng(**data_store)
+print("test data shape:\t", df_test.shape)
+
+# Drop the insignificant features
+df_train = df_train.pipe(Pipeline.filter_cols)
+df_test = df_test.select([col for col in df_train.columns if col != "target"])
+
+print("train data shape:\t", df_train.shape)
+print("test data shape:\t", df_test.shape)
+
+df_train, cat_cols = to_pandas(df_train)
+df_test, cat_cols = to_pandas(df_test, cat_cols)
+
+del data_store
+gc.collect()
+
+print("Train is duplicated:\t", df_train["case_id"].duplicated().any())
+print("Train Week Range:\t", (df_train["WEEK_NUM"].min(), df_train["WEEK_NUM"].max()))
+print()
+print("Test is duplicated:\t", df_test["case_id"].duplicated().any())
+print("Test Week Range:\t", (df_test["WEEK_NUM"].min(), df_test["WEEK_NUM"].max()))
+
+X = df_train.drop(columns=["target", "case_id", "WEEK_NUM"])
+y = df_train["target"]
+weeks = df_train["WEEK_NUM"]
+
+cv = StratifiedGroupKFold(n_splits=5, shuffle=False)
+
+params = config
+
+fitted_models = []
+cv_scores = []
+
+for idx_train, idx_valid in cv.split(X, y, groups=weeks):
+    X_train, y_train = X.iloc[idx_train], y.iloc[idx_train]
+    X_valid, y_valid = X.iloc[idx_valid], y.iloc[idx_valid]
+
+    model = lgb.LGBMClassifier(**params)
+    model.fit(
+        X_train, y_train,
+        eval_set=[(X_valid, y_valid)],
+        callbacks=[lgb.log_evaluation(200), lgb.early_stopping(50)])
+    wandb.log({"best_iteration": model.best_iteration_})
+    wandb.log({"best_score": model.best_score_})
+    # modelをwandbへ保存
+    wandb.save("model.pickle")
+    with open(os.path.join(wandb.run.dir, "model.pickle"), 'wb') as f:
+        pickle.dump(model, f)
+
+    fitted_models.append(model)
+
+    y_pred_valid = model.predict_proba(X_valid)[:, 1]
+    auc_score = roc_auc_score(y_valid, y_pred_valid)
+    wandb.log({"auc_score": auc_score})
+    cv_scores.append(auc_score)
+
+print("CV AUC scores: ", cv_scores)
+print("Maximum CV AUC score: ", max(cv_scores))
+
+X_test = df_test.drop(columns=["WEEK_NUM"])
+X_test = X_test.set_index("case_id")
+
+lgb_pred = pd.Series(model.predict_proba(X_test)[:, 1], index=X_test.index)
+
+df_subm = pd.read_csv(ROOT / "sample_submission.csv")
+df_subm = df_subm.set_index("case_id")
+
+df_subm["score"] = lgb_pred
+
+df_subm.head()
+
+df_subm.to_csv(f"submission_{datetime.now().strftime('%Y%m%d%H%M%S')}.csv", index=False)
